d235d7d54968244fb10b8949a7e5800b79b5d82c
hongyukeji <huan1314!>
2017-01-21 00:56:39 +0800

    Html5

--- eBestMall/static/js/main.js
+++ eBestMall/static/js/main.js
@@ -16,3 +16,23 @@ require(['jquery', 'system'], function($, system){
     console.log( system.systemDate() );
 });
 */
+
+$(function(){
+    var fixedTop = $('.fixedTop'),
+        topRight = $('.topRight');
+
+    $(window).trigger('scroll');
+    $(window).on('scroll', function(){
+        if($(window).scrollTop() > $(window).height()){
+            fixedTop.fadeIn();
+            topRight.fadeIn();
+        }else{
+            fixedTop.fadeOut();
+            topRight.fadeOut();
+        }
+    });
+
+    topRight.on('click', function(){
+        $('html,body').animate({scrollTop: 0},588);
+    });
+});
